014ae6baa66da38b08a4f817f87b9d818d113ce7
bhargava562 <bhargavaak45@gmail.com>
2025-06-16 10:22:28 +0530

    Added the Middle Info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { Routes } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+
 
 function App() {
 
   return (
     <Routes>
+        <Route path="/" element={<Home />} />
     </Routes>
+    
   )
 }
 
