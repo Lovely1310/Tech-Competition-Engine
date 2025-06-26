eecfbf46fd9f1f3d3a5a2088aab45de9fbee7c5b
Bhargava <bhargavaak45@gmail.com>
2025-06-26 19:56:28 +0530

    added business login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './index.css';
 import { Routes,Route,BrowserRouter } from 'react-router-dom'
 import './App.css'
 import LandingPage from './Pages/LandingPage'
+import BusinessLogin from './Pages/BusinessLogin'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/work/login" element={<BusinessLogin.jsx />} />
         </Routes>
     </BrowserRouter>
     
