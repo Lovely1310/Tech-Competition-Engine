5ad93a67c38483f9a10490aef6b1b7c96200c096
Shreyaoshi Das <sdas200010@gmail.com>
2025-07-03 12:55:10 +0530

    added changes

--- frontend/src/components/loginpage/Loginmiddle.jsx
+++ frontend/src/components/loginpage/Loginmiddle.jsx
@@ -26,7 +26,7 @@ const Loginmiddle = () => {
        <div className="hidden md:block h-[300px] w-px bg-gray-300 mx-6"></div>
        <div className=" md:block h-[1px] w-[80%] bg-gray-300 mx-6 md:hidden lg:hidden"></div>
         
-        <div className=' flex flex-col  gap-7 items-center justify-center p-5 pt-26 lg-w-1/2  lg-w-1/2'>
+        <div className=' flex flex-col  gap-7 items-center justify-center p-5 pt-22 lg-w-1/2  lg-w-1/2'>
           <h2 className='text-[2rem] font-bold'>For Coders</h2>
           <p className=' w-[70%]'>Connect with a global community of 26+ million developers. Sharpen your coding skills, ace technical interviews, and unlock new career opportunities.
           </p>
