646f16ca291d22b6920a61ec955c2dc7d84de05d
blackcross7 <kartikay.kk47@gmail.com>
2025-07-07 21:59:27 +0530

    File structure corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,8 @@ import LandingPage from './Pages/LandingPage'
 import BusinessLogin from './Pages/BusinessLogin'
 import LoginPage from './components/LoginPageComponents/LoginPage';
 import Footer from './components/Footer';
-import PageAfterLogin from './Pages/PageAfterLogin';
 import DeveloperLogin from './components/LoginPageComponents/DeveloperLogin';
 import Apply from './Pages/Apply';
-import CertifySection from './components/pageAfterlogin/CertifySection';
 import DeveloperDashboard from  './Pages/Dashboard'
 
 
@@ -20,11 +18,9 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/work/login" element={<BusinessLogin />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<PageAfterLogin />} />
             <Route path="/login/developerlogin" element={<DeveloperLogin />} />
             <Route path='/apply' element={<Apply />}></Route>
             <Route path="/skills-verification" element={<CertifySection />} />
-            <Route path="/DeveloperLanding" element={<PageAfterLogin />} />
             <Route path="/DeveloperDashboard" element={<DeveloperDashboard />} />
 
         </Routes>
