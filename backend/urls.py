f2a08b098c279a42c9422cbaf282b33caaa02bf3
AishwaryaMedhe <aishwaryamedhe5@gmail.com>
2025-07-14 20:21:42 +0530

    pushed for login authentication.Need to check

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 def home(request):
     return HttpResponse("🎉 Welcome to the HackerRank Clone Backend API!")
@@ -24,7 +26,7 @@ def home(request):
 urlpatterns = [
      path('', home),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  
+    path('api/', include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 ]
-
-
